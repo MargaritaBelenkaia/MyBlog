4130db63fc0b522a02748889909112f839411e2a
Margarita Belenkaia <margarita.belenkaia@gmail.com>
2017-10-13 22:49:45 +0300

    register client + server

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
-// import { HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -21,9 +22,9 @@ import { RegisterComponent } from './components/register/register.component';
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    // HttpModule
+    HttpModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
